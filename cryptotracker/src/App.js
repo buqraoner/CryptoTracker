18626a278f4a73b0982b9a66db34c6bc5dc57b58
Buğra Öner <boletroy@gmail.com>
2022-03-31 20:12:56 +0300

    ...

--- cryptotracker/src/App.js
+++ cryptotracker/src/App.js
@@ -4,18 +4,40 @@ import * as React from "react";
 import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 import TrendingData from "./components/Trending/TrendingData";
 import MarketData from "./components/Market/MarketData";
+import { useMediaQuery } from 'react-responsive';
+
 
 
 function App() {
+  /*
+  const isDesktop = useMediaQuery({
+    query: "(min-width: 1224px)"
+  });
+  const isTablet = useMediaQuery({
+    query: "(max-width: 1224px)"
+  });
+  const isMobile = useMediaQuery({
+    query: "(max-width: 786px)"
+  });
+  const isPortrait = useMediaQuery({
+    query: "(orientation: portrait)"
+  });
+  const isRetina = useMediaQuery({
+    query: "(max-resolution: 300dpi)"
+  });
+  */
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<CoinData />} />
-          <Route path="/Trending" element={<TrendingData />} />
-          <Route path="/Market" element={<MarketData />} />
-        </Routes>
-      </BrowserRouter>
+      <div>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<CoinData />} />
+            <Route path="/Trending" element={<TrendingData />} />
+            <Route path="/Market" element={<MarketData />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
